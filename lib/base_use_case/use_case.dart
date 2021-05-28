f70049e3a3e654bf4454ec41f337ffd50b83fc74
ionolaru <spideiron@live.it>
2021-05-29 00:22:14 +0200

    - add usecase

---
+++ lib/base_use_case/use_case.dart
@@ -0,0 +1,13 @@
+// Parameters have to be put into a container object so that they can be
+// included in this abstract base class method definition.
+
+import 'package:clean_architecture/base_repository/base_repository.dart';
+
+/// The useCase mainly is in charge of managing the EBR objects
+/// returns [BusinessError] when request is not correct
+/// has reference to a repository
+abstract class UseCase<ResultModel, RequestModel, T extends BaseRepository?> {
+  final T repository;
+
+  UseCase(this.repository);
+}
