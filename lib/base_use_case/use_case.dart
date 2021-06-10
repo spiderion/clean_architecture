46764ee065dfc396ff8c492fe07b3c0411db5fb9
ionolaru <spideiron@live.it>
2021-06-11 00:16:32 +0200

    - remove

--- lib/base_use_case/use_case.dart
+++ lib/base_use_case/use_case.dart
@@ -7,8 +7,6 @@ import 'package:clean_architecture/request_and_response_model/request_behaviour.
 /// The useCase mainly is in charge of managing the EBR objects
 /// returns [BusinessError] when request is not correct
 /// has reference to a repository
-/// user [RxUseCase] instead with [RequestObserver] abstract class UseCase<ResultModel, RequestModel, T extends BaseRepository?>
-@deprecated
 abstract class UseCase<ResultModel, RequestModel, T extends BaseRepository?> {
   final T repository;
 
