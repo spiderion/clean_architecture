6a729d4fa0f7514951e8bf1f128849cf201cb2f2
ionolaru <spideiron@live.it>
2021-06-11 00:16:43 +0200

    - remove

--- lib/base_use_case/use_case.dart
+++ lib/base_use_case/use_case.dart
@@ -2,7 +2,6 @@
 // included in this abstract base class method definition.
 
 import 'package:clean_architecture/base_repository/base_repository.dart';
-import 'package:clean_architecture/request_and_response_model/request_behaviour.dart';
 
 /// The useCase mainly is in charge of managing the EBR objects
 /// returns [BusinessError] when request is not correct
