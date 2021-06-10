f19171f5bd338e52d9e4595b9763963a6cc45b57
ionolaru <spideiron@live.it>
2021-06-10 23:24:14 +0200

    - add rx usecase

--- lib/base_use_case/use_case.dart
+++ lib/base_use_case/use_case.dart
@@ -2,12 +2,28 @@
 // included in this abstract base class method definition.
 
 import 'package:clean_architecture/base_repository/base_repository.dart';
+import 'package:clean_architecture/request_and_response_model/request_behaviour.dart';
 
 /// The useCase mainly is in charge of managing the EBR objects
 /// returns [BusinessError] when request is not correct
 /// has reference to a repository
+/// user [RxUseCase] instead with [RequestObserver] abstract class UseCase<ResultModel, RequestModel, T extends BaseRepository?> {
 abstract class UseCase<ResultModel, RequestModel, T extends BaseRepository?> {
   final T repository;
 
   UseCase(this.repository);
+}
+
+abstract class RxUseCase<T extends BaseRepository?> {
+  final T repository;
+
+  RxUseCase(this.repository);
+
+  Future<void> get<RQ, RP>(RequestObserver<RQ, RP> observer) async {
+    throw UnimplementedError('method must be implemented before use');
+  }
+
+  Future<void> set<RQ, RP>(RequestObserver<RQ, RP> observer) async {
+    throw UnimplementedError('method must be implemented before use');
+  }
 }
