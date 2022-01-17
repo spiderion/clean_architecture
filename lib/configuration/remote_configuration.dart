33b9baa71c600652ef3ca5bdb1a749782b19f8fa
ionolaru <spideiron@live.it>
2022-01-17 12:28:49 +0100

    - add remote config methods

--- lib/configuration/remote_configuration.dart
+++ lib/configuration/remote_configuration.dart
@@ -1,5 +1,11 @@
 abstract class RemoteConfiguration {
   String getString(String key);
 
+  bool getBool(String key);
+
+  double getDouble(String key);
+
+  int getInt(String key);
+
   Future<bool> fetchLatest();
 }
