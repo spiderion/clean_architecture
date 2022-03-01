4682f195c7609f70563b0a71cc501dc5f8f7f74e
ionolaru <spideiron@live.it>
2022-03-01 09:43:50 +0100

    - remove exception captor

--- lib/error/error.dart
+++ lib/error/error.dart
@@ -1,9 +1,8 @@
-import 'package:clean_architecture/request_and_response_model/result_model.dart';
 import 'package:equatable/equatable.dart';
 
 ///This is the final error returned from the [UseCase] which will be
 ///handled by an [ErrorStateResolver]
-abstract class Error extends Equatable implements ResultModel {
+abstract class Error extends Equatable {
   final String? message;
   final String? code;
 
@@ -42,14 +41,12 @@ class PermissionError extends Error {
 }
 
 class FirstTimePermissionError extends Error {
-  FirstTimePermissionError(
-      {String message = "PERMISSION_TO_USE_THIS_FEATURE_ARE_NOT_GRANTED", String? code})
+  FirstTimePermissionError({String message = "PERMISSION_TO_USE_THIS_FEATURE_ARE_NOT_GRANTED", String? code})
       : super(message: message, code: code);
 }
 
 class CaseNotFoundError extends Error {
-  CaseNotFoundError({String message = "DEVELOPER_ERROR", String? code})
-      : super(message: message, code: code);
+  CaseNotFoundError({String message = "DEVELOPER_ERROR", String? code}) : super(message: message, code: code);
 }
 
 class UnimplementedError extends Error {
